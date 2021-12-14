5935256113f35dfd5cce8591f52e18d30ae1192d
Samuel Martins <francisco.martins@vaivoa.com>
2021-12-14 10:20:54 -0300

    Initial Commit

---
+++ Shared/DesafioGitUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioGitUiApp.swift
+//  Shared
+//
+//  Created by FRANCISCO SAMUEL DA SILVA MARTINS on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DesafioGitUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
