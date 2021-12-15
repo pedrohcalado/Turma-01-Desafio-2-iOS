08acd92b72cecd3fe86001b8dfdc4ae147207b24
Pedro Calado <pedrocalado22@gmail.com>
2021-12-15 16:52:58 -0300

    creates cell

--- Desafio 2/Desafio 2/ContentView.swift
+++ Desafio 2/Desafio 2/ContentView.swift
@@ -7,10 +7,67 @@
 
 import SwiftUI
 
+struct RepositoriesListView: View {
+    var body: some View {
+        NavigationView {
+            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image("github")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50, alignment: .trailing)
+                        VStack(alignment: .leading, spacing: 3) {
+                            HStack {
+                                Text("Name: ")
+                                    .fontWeight(.semibold)
+                                Text("Repository")
+                            }
+                            HStack {
+                                Text("Author: ")
+                                    .fontWeight(.semibold)
+                                Text("Author Name")
+                            }
+                        }
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        HStack {
+                            HStack {
+                                Text("Stars:")
+                                    .fontWeight(.semibold)
+                                Text("10")
+                            }
+                            HStack {
+                                Text("Forks: ")
+                                    .fontWeight(.semibold)
+                                Text("132")
+                            }
+                        }
+                        
+                        HStack {
+                            Text("Description:")
+                                .fontWeight(.semibold)
+                            Text("dasdsadbvcbcvbcvbcvxbcvbxccvbxcvbvxcvzxvcvcxvvcxvcxvxcvxcvcxasdsa")
+                                .lineLimit(2)
+                                .minimumScaleFactor(0.9)
+                        }
+                        
+                    }
+                }
+            }
+            .navigationBarTitle("Swift Repositories")
+        }
+        
+        
+    }
+}
+
+// Nome do repositório, Descrição do Repositório, Nome / Foto do autor, Número de Stars, Número de Forks
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        RepositoriesListView()
     }
 }
 
