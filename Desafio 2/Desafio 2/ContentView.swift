d8c8a21e37359d62692c43fadd2d869a35a6e5ea
pedrohcalado <pedrocalado22@gmail.com>
2021-12-15 10:53:36 -0300

    initial commit

---
+++ Desafio 2/Desafio 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Desafio 2
+//
+//  Created by Pedro Henrique Calado on 15/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Desafio 2/Desafio 2/Desafio_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_2App.swift
+//  Desafio 2
+//
+//  Created by Pedro Henrique Calado on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
