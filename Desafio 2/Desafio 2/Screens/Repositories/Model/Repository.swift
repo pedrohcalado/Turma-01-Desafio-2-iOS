14c6562dffa70ed415f4c77ae51bf3777dda1171
Pedro Calado <pedrocalado22@gmail.com>
2021-12-15 17:32:33 -0300

    includes info.plist infos to fix ios version problem

---
+++ Desafio 2/Desafio 2/Screens/Repositories/Model/Repository.swift
@@ -0,0 +1,8 @@
+//
+//  Repository.swift
+//  Desafio 2
+//
+//  Created by PEDRO HENRIQUE CALADO on 12/15/21.
+//
+
+import Foundation
---
+++ Desafio 2/Desafio 2/Screens/Repositories/View/RepositoriesView.swift
@@ -0,0 +1,8 @@
+//
+//  RepositoriesView.swift
+//  Desafio 2
+//
+//  Created by PEDRO HENRIQUE CALADO on 12/15/21.
+//
+
+import Foundation
