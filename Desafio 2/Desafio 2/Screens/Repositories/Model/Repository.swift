e57d6a71dd4e5fe35a3aec9e6dee502ceef4dbc7
Pedro Calado <pedrocalado22@gmail.com>
2021-12-15 21:17:53 -0300

    create models

--- Desafio 2/Desafio 2/Screens/Repositories/Model/Repository.swift
+++ Desafio 2/Desafio 2/Screens/Repositories/Model/Repository.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+typealias Repositories = [Repository]
+
+struct Repository: Codable {
+    let repositoryName: String
+    let owner: Owner
+    let stars: Int
+    let forks: Int
+    let description: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case repositoryName = "name"
+        case owner
+        case stars = "stargazers_count"
+        case forks = "forks_count"
+        case description
+    }
+    
+}
+
+struct GitHubReponse: Codable {
+    let repositories: Repositories
+    
+    enum CodingKeys: String, CodingKey {
+        case repositories = "items"
+    }
+}
+
+struct Owner: Codable {
+    let ownerName: String
+    let profilePicture: String
+    
+    enum CodingKeys: String, CodingKey {
+        case ownerName = "login"
+        case profilePicture = "avatar_url"
+    }
+}
