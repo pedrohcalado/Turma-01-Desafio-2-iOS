fc294ed79686ab7dc5e0ab3c5d414da58e382a5f
pedrohcalado <pedrocalado22@gmail.com>
2021-12-16 08:17:06 -0300

    create pull requests service

--- Desafio 2/Desafio 2/Services/GitHubService.swift
+++ Desafio 2/Desafio 2/Services/GitHubService.swift
@@ -20,4 +20,16 @@ class GitHubService: RepositoriesService {
             }
         }
     }
+    
+    func getPullRequests(fromRepository repositoryName: String, ownedBy repositoryOwner: String, completion: @escaping (Bool, GitHubReponse?, String?) -> Void) {
+        let request = AF.request("https://api.github.com/repos/\(repositoryOwner)/\(repositoryName)/pulls")
+        request.validate().responseDecodable(of: GitHubReponse.self) { response in
+            switch response.result {
+            case .success:
+                completion(true, response.value, nil)
+            case .failure(let error):
+                completion(false, nil, "Error when fetching pull requests: \(error)")
+            }
+        }
+    }
 }
